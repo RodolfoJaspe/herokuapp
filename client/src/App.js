6e6a406286cf82496c9ddc063bf1a6f87e91cf73
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-06-10 13:58:49 -0400

    monolith

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import {useState, useEffect} from "react"
 
 function url(path){
-    return process.env.NODE_ENV = "development" ? `http://localhost:1234/${path}`:path
+    return process.env.NODE_ENV == "development" ? `http://localhost:1234${path}`:path
 }
 
 function App() {
-  const {data, setData} = useState("Hi")
+  const [data, setData] = useState("Hi")
   useEffect(()=>{
       fetch(url("/api/"))
       .then(res=>res.json())
